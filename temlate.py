44e8a27e855fe185054d6d0c236cf36e0c0242bb
feikon <hpuyanfeng@126.com>
2017-06-07 21:20:25 +0800

    add python property

---
+++ temlate.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: CZYF
+@license: Apache Licence 
+@contact: crossfirestarer@gmail.com
+@site: https://github.com/feikon
+@software: PyCharm
+@file: temlate.py
+@time: 2017/5/18 9:11
+"""
+
+import time
+
+
+
+def func():
+    pass
+
+
+def programExeTime():
+    startTime = time.clock()
+    for i in range(1, 20000)
+
+
+class Main:
+    def __init__(self):
+        pass
+    a = []
+
+
+
+if __name__ == '__main__':
+    pass
+
