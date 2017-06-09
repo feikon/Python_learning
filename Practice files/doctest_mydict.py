0d380714d20fa202ec13dad0367514dad4c5538a
feikon <hpuyanfeng@126.com>
2017-06-09 10:55:42 +0800

    add doctest file

---
+++ Practice files/doctest_mydict.py
@@ -0,0 +1,56 @@
+# !/usr/bin/env python3
+# encoding: utf-8
+
+
+"""
+@version: 0.1
+@author: feikon
+@license: Apache Licence 
+@contact: crossfirestarer@gmail.com
+@site: https://github.com/feikon
+@software: PyCharm
+@file: doctest_mydict.py
+@time: 2017/6/9 10:52
+"""
+
+
+class Dict(dict):
+    """
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    """
+
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Python_learning
 Some practice python files.
 
-- inherit_poly.py 
+- inherit_poly.py    
 关于继承和多态的练习，里面还有@staticmethod的使用
 
-- python_decorator 
+- python_decorator    
 装饰器的使用，带参数和不带参数的示例,以及解决函数签名问题的引入的functools中的 @functools.wraps(func)的使用
 
-- python_property  
+- python_property    
 @property的使用，还有疑问待解决，property作用，为什么用它
 
 - enum_class       
