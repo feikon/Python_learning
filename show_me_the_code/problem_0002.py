c7154d11bb5384f11d87152914f25b0dd6e5d3f7
feikon <hpuyanfeng@126.com>
2017-06-12 09:45:53 +0800

    插入激活码到MySQL

--- show_me_the_code/problem_0002.py
+++ show_me_the_code/problem_0002.py
@@ -13,6 +13,53 @@
 @time: 2017/6/11 15:24
 """
 
+# Problem describe:generate active code to mysql
+# Problem solve step:
+# 1.Connect to mysql;
+# 2.Insert generate code;
 
 
+import pymysql.cursors
+import pymysql
+import uuid
+
+
+def generate_activation_code(num):
+    code_list = []
+    for i in range(num):
+        code = str(uuid.uuid4()).replace('-', '').upper()
+        while code in code_list:
+            code = str(uuid.uuid4()).replace('-', '').upper()
+        code_list.append(code)
+
+    return code_list
+
+
+def store_in_mysql(code_list):
+    try:
+        conn = pymysql.connect(host='127.0.0.1', user='root', passwd='518518', db='mysql')
+        cur = conn.cursor()
+    except BaseException as e:
+        print(e)
+    else:
+        try:
+            cur.execute('CREATE DATABASE IF NOT EXISTS activation_code')
+            cur.execute('USE activation_code')
+            cur.execute('''CREATE TABLE IF NOT EXISTS code(
+                            id INT NOT NULL AUTO_INCREMENT,
+                            code VARCHAR(32) NOT NULL,
+                            PRIMARY KEY(id)
+                        )''')
+            for code in code_list:
+                cur.execute('INSERT INTO code(code) VALUES({})'.format(code))
+                cur.connection.commit()
+        except BaseException as e:
+            print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+if __name__ == '__main__':
+    store_in_mysql(generate_activation_code(200))
+    print('OK!')
 
