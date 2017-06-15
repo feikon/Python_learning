09221809c4827a7626cb22bc5b4474dc796b138f
feikon <hpuyanfeng@126.com>
2017-06-15 16:31:10 +0800

    solve problem_0009 print link

--- show_me_the_code/problem_0009.py
+++ show_me_the_code/problem_0009.py
@@ -13,6 +13,24 @@
 @time: 2017/6/15 16:19
 """
 
+# Problem describe:Prase a html then find link
+# Problem solve step:
+# 1.Praser html;
+# 2.Print link;
 
+from bs4 import BeautifulSoup
+import requests
+
+
+def print_link(url):
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'lxml')
+    print(soup.prettify())
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+
+
+if __name__ == '__main__':
+    print_link('http://www.jianshu.com/p/4ad744ed955c')
 
 
